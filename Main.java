f9802088c79eb8ad0cbee2e34c3b3e1cca47a8fc
ABIRAMI S <112795045+Abi-123-hub@users.noreply.github.com>
2022-09-12 15:25:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter two numbers");
+	    int first = 10;
+	    int second = 20; 
+	    System.out.println(first + " " + second);
+	    int sum = first + second;
+	    System.out.println("The sum is: " + sum);
+	}
+
+}
---
+++ Main4.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    char c = 'a';
+    if (Character.isAlphabetic(c)) {
+      System.out.println(c + " is an alphabet.");
+    }
+    else {
+      System.out.println(c + " is not an alphabet.");
+    }
+  }
+}
